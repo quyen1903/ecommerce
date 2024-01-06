d7ba0b7a1c25a731a99d051d387c5120e93b1aa6
quyen1903 <quyenng2k@gmail.com>
2024-01-06 15:57:23 +0700

    dotenv file and connect to database

--- source/app.js
+++ source/app.js
@@ -1,19 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const helmet = require('helmet');
+const { default:helmet } = require('helmet');
 const compression = require('compression');
+
 //init middleware
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(compression());
 //init database
+require('./dbs/init.mongodb');
+
+//init routes
 app.get('/',(req,res,next)=>{
     return res.status(200).json({
         message:'welcome'
     })
 })
-//init routes
-
 //error handling
 module.exports=app
---
+++ source/routes/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const express = require('express')
+const { route } = require('../app')
+const router = express.Router()
+
+router.use
+module.exports=router
