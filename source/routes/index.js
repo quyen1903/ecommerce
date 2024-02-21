2478e80380e353b76696493b7c0f92d2a97b5cc2
quyen1903 <quyenng2k@gmail.com>
2024-02-21 10:15:37 +0700

    api key version

--- source/routes/index.js
+++ source/routes/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const express = require('express');
+const { apiKey,permission } = require('../auth/checkAuth');
 const router = express.Router();
 
+//check api key
+router.use(apiKey)
+//check permission
+router.use(permission('0000'))
+
 router.use('/v1/api',require('./access'))
-// router.get('/',(req,res,next)=>{
-//     return res.status(200).json({
-//         message:'welcome'
-//     })
-// });
+
 module.exports=router
